31a15c5dc40d4b1fa255a14902d97ec5fea641d2
Designer-space <123vinaymaurya@gmail.com>
2024-09-20 11:21:23 +0530

    fixed the error handler and typo

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,64 +1,67 @@
-import { compare } from "bcrypt"
-import { User } from "../models/user.js"
-import { generateTokenAndSetCookie } from "../utils/generateToken.js"
-import { TryCatch } from "../middlewares/error.js"
-import { ErrorHandler } from "../utils/utility.js"
+import { compare } from "bcrypt";
+import { User } from "../models/user.js";
+import { generateTokenAndSetCookie } from "../utils/generateToken.js";
+import { TryCatch } from "../middlewares/error.js";
+import { ErrorHandler } from "../utils/utility.js";
 
 export const signup = TryCatch(
   async (req, res, next) => {
 
-    const { name, username, password, bio } = req.body
+    const { name, username, password, bio } = req.body;
 
-    const file = req.file
+    const file = req.file;
 
-    if (!file) return next(new ErrorHandler("Please Upload Profile Picture", 400))
+    if (!file) return next(new ErrorHandler("Please Upload Profile Picture", 400));
 
     const avatar = {
       public_id: "asdfgh",
       url: "http://helloworld.com"
-    }
+    };
 
     const user = await User.create({
       name, username, password, bio, avatar
-    })
+    });
 
-    generateTokenAndSetCookie(user._id, res)
+    generateTokenAndSetCookie(user._id, res);
 
     res.status(201).json({
       message: "User Created Successfully"
-    })
+    });
 
   }
-)
+);
 export const login = TryCatch(
   async (req, res, next) => {
 
     const { username, password } = req.body;
 
-    const user = await User.findOne({ username }).select("+password")
-    const isMatch = await compare(password, user.password)
+    const user = await User.findOne({ username }).select("+password");
 
-    if (!user || !isMatch) return next(new ErrorHandler("Invalid Credentials", 404))
+    if (!user) return next(new ErrorHandler("Invalid Username or password", 404));
 
-    generateTokenAndSetCookie(user._id, res)
+    const isMatch = await compare(password, user.password);
+
+    if (!isMatch) return next(new ErrorHandler("Invalid Username or password", 404));
+
+    generateTokenAndSetCookie(user._id, res);
 
     res.status(201).json({
       message: "Login Successfully"
-    })
+    });
 
   }
-)
+);
 
 export const logout = (req, res) => {
   res.cookie("convoia-token", "", {
     maxAge: 0
-  })
-  return res.status(200).json({ message: "Logout Succesfully" })
-}
+  });
+  return res.status(200).json({ message: "Logout Succesfully" });
+};
 
 export const getme = (req, res) => {
 
-  const user = req.user
+  const user = req.user;
 
-  return res.status(200).json(user)
-}
+  return res.status(200).json(user);
+};
