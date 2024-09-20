9a56b9a6231381e3b13ddbaf4f3dd9f4ff316f3e
Designer-space <123vinaymaurya@gmail.com>
2024-09-20 11:21:51 +0530

    fixed the typo

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { Server } from "socket.io";
 import { createServer } from "http";
@@ -9,15 +9,15 @@ import cors from "cors";
 import connectMongoDB from "./utils/connectMongoDB.js";
 import { errorMiddleware, TryCatch } from "./middlewares/error.js";
 
-import authRoutes from "./routes/authRoutes.js"
-import userRoutes from "./routes/userRoutes.js"
-import chatRoutes from "./routes/chatRoutes.js"
-import adminRoutes from "./routes/adminRoutes.js"
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import chatRoutes from "./routes/chatRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
 import { NEW_MESSAGE, NEW_MESSAGE_ALERT } from "./constants/events.js";
 import { getSockets } from "./lib/helper.js";
 import { Message } from "./models/message.js";
 
-dotenv.config()
+dotenv.config();
 
 const PORT = process.env.PORT || 300;
 
@@ -26,38 +26,38 @@ export const userSocketIDs = new Map();
 
 connectMongoDB();
 
-const app = express()
+const app = express();
 
-const server = createServer(app)
+const server = createServer(app);
 
-const io = new Server(server, {})
+const io = new Server(server, {});
 
 // Middleware
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 app.use(cors({
   origin: ["http://localhost:5173", "http://localhost:4173", process.env.CLIENT_URL],
   credentials: true
-}))
+}));
 
 
 // API Routes
-app.use("/api/v1/auth", authRoutes)
-app.use("/api/v1/user", userRoutes)
-app.use("/api/v1/chat", chatRoutes)
-app.use("/api/v1/admin", adminRoutes)
-app.use(errorMiddleware)
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/chat", chatRoutes);
+app.use("/api/v1/admin", adminRoutes);
+app.use(errorMiddleware);
 
-io.use(() => { })
+io.use(() => { });
 
 io.on("connection", (socket) => {
 
   const tempUser = {
     _id: uuid(),
     name: "Guest",
-  }
+  };
 
-  userSocketIDs.set(tempUser._id.toString(), socket.id)
+  userSocketIDs.set(tempUser._id.toString(), socket.id);
 
   console.log(userSocketIDs);
 
@@ -72,40 +72,40 @@ io.on("connection", (socket) => {
       },
       chat: chatId,
       createdAt: new Date().toISOString(),
-    }
+    };
 
     const messageForDB = {
       content: message,
       sender: tempUser._id,
       chat: chatId,
-    }
+    };
 
-    const usersSocket = getSockets(members)
+    const usersSocket = getSockets(members);
 
     io.to(usersSocket).emit(NEW_MESSAGE, {
       chatId,
       message: messageForRealtime
-    })
+    });
 
     io.to(usersSocket).emit(NEW_MESSAGE_ALERT, {
       chatId
-    })
+    });
 
     TryCatch(async () => {
-      await Message.create(messageForDB)
-    })
-  })
+      await Message.create(messageForDB);
+    });
+  });
 
   socket.on("disconnect", () => {
     console.log("User Disconnected");
-    userSocketIDs.delete(tempUser._id.toString())
-  })
-})
+    userSocketIDs.delete(tempUser._id.toString());
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
-})
+});
 
 server.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
-})
+});
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,11 +4,11 @@ import { singleAvatar } from "../middlewares/multer.js";
 import { protectRoutes } from "../middlewares/protectroutes.js";
 import { loginValidator, signUpValidator, validateHandler } from "../lib/validators.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("/signup", singleAvatar, signUpValidator(), validateHandler, signup)
-router.post("/login", loginValidator(), validateHandler, login)
-router.get("/logout", logout)
-router.get("/me", protectRoutes, getme)
+router.post("/signup", singleAvatar, signUpValidator(), validateHandler, signup);
+router.post("/login", loginValidator(), validateHandler, login);
+router.get("/logout", logout);
+router.get("/me", protectRoutes, getme);
 
-export default router
+export default router;
