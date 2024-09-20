f321b9cdab76fd7fec6febf07c73a1587a6b6185
Designer-space <123vinaymaurya@gmail.com>
2024-09-20 11:20:04 +0530

    add extra error code and fixed the typo

--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -1,30 +1,31 @@
 const errorMiddleware = (err, req, res, next) => {
 
-  err.message ||= "Internal server Error",
-    err.statusCode ||= 500
-
   if (err.name === "CastError" && err.kind === "ObjectId") {
     err.statusCode = 404;
-    err.message = `Invalid Format of ${err.path}`
+    err.message = `Invalid Format of ${err.path}`;
   }
 
   if (err.code === 11000) {
-    err.statusCode = 400
-    err.message = `${Object.keys(err.keyValue)} field has to be unique`
+    err.statusCode = 400;
+    err.message = `${Object.keys(err.keyValue)} field has to be unique`;
   }
+
+  err.message ||= "Internal server Error",
+    err.statusCode ||= 500;
+
 
   return res.status(err.statusCode).json({
     message: err.message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack
-  })
-}
+  });
+};
 
 const TryCatch = (func) => async (req, res, next) => {
   try {
-    await func(req, res, next)
+    await func(req, res, next);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
-export { errorMiddleware, TryCatch }
+export { errorMiddleware, TryCatch };
